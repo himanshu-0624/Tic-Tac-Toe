9d870056071284a80176bafba05599f07b37c2ff
Himanshu Gupta <himbxr22@gmail.com>
2020-10-28 18:24:37 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,113 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
+import Icon from './component/icon';
+
+import {Card, CardBody, Container, Button, Col, Row} from "reactstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
 
-function App() {
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const itemArray = new Array(9).fill("empty");
+
+const App = () =>  {
+
+  const [isCross, setIsCross] = useState(false);
+  const [winMessage, setWinMessage] = useState("");
+
+  const reloadGame = () => {
+    setIsCross(false);
+    setWinMessage("")
+    itemArray.fill("empty", 0 , 9);
+  }
+  const checkIsWinner = () => {
+    if(itemArray[0] == itemArray[1] 
+      && itemArray [0] == itemArray[2] 
+      && itemArray[0] != "empty"){
+        setWinMessage(`${itemArray[0]} wins`)
+      }
+       else if(itemArray[0] == itemArray[3] 
+        && itemArray [0] == itemArray[6] 
+        && itemArray[0] != "empty"){
+          setWinMessage(`${itemArray[0]} wins`)
+      }
+      else if(itemArray[0] == itemArray[4] 
+        && itemArray [0] == itemArray[8] 
+        && itemArray[0] != "empty"){
+          setWinMessage(`${itemArray[0]} wins`)
+      }
+      else if(itemArray[1] == itemArray[4] 
+        && itemArray [1] == itemArray[7] 
+        && itemArray[1] != "empty"){
+          setWinMessage(`${itemArray[1]} wins`)
+      }
+      else if(itemArray[2] == itemArray[5] 
+        && itemArray [2] == itemArray[8] 
+        && itemArray[2] != "empty"){
+          setWinMessage(`${itemArray[2]} wins`)
+      }
+      else if(itemArray[2] == itemArray[4] 
+        && itemArray [2] == itemArray[6] 
+        && itemArray[2] != "empty"){
+          setWinMessage(`${itemArray[2]} wins`)
+      }
+      else if(itemArray[3] == itemArray[4] 
+        && itemArray [3] == itemArray[5] 
+        && itemArray[3] != "empty"){
+          setWinMessage(`${itemArray[3]} wins`)
+      }
+      else if(itemArray[6] == itemArray[7] 
+        && itemArray [6] == itemArray[8] 
+        && itemArray[6] != "empty"){
+          setWinMessage(`${itemArray[6]} wins`)
+      }
+  } 
+
+  const changeItem = itemNumber => {
+  if(winMessage) {
+    return toast(winMessage, {type: "success"})
+  }
+  
+  if(itemArray[itemNumber] == "empty"){
+          itemArray[itemNumber] = isCross ? "cross" : "circle";
+          setIsCross(!isCross);
+  } else{
+    return toast("already filled", {type: "error"})
+  }
+  checkIsWinner();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="p-5">
+          <ToastContainer position="bottom-center" />
+          <Row>
+            <Col md={6} className="offset-md-3">
+              {winMessage ? (
+                <div className="mb-2 mt-2 ">
+                  <h1 className="text-success text-uppercase text-center">
+                    {winMessage}
+                  </h1>
+                  <Button color="success" block onClick={reloadGame}>Reload the game</Button>
+                </div>
+              ) : (
+                <h1 className="text-center text-warning">
+                  {isCross ? "Cross" : "Circle"} turns
+                </h1>
+              ) }
+              <div className="grid">
+                {itemArray.map((item, index) => (
+                  <Card color="warning"  onClick={() => changeItem(index) }>
+                    <CardBody className="box">
+                      <Icon name={item} />
+                    </CardBody>
+                  </Card>
+                ))}
+              </div>
+            </Col>
+            </Row> 
+    </Container>
   );
 }
 
